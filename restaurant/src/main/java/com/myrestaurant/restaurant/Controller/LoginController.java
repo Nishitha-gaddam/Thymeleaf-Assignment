9c7d08fbc730bf2feb493da482a2dced404aaac1
Nishitha Gaddam <nishitha.gaddam@zemosolabs.com>
2020-06-04 20:32:54 +0530

    made required changes

--- restaurant/src/main/java/com/myrestaurant/restaurant/Controller/LoginController.java
+++ restaurant/src/main/java/com/myrestaurant/restaurant/Controller/LoginController.java
@@ -3,21 +3,29 @@ package com.myrestaurant.restaurant.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class LoginController {
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
 
-	 @GetMapping("/")
-	    public String welcomePage() {
-	        return "welcome";
-	    }
-	 @GetMapping("/thank-you")
-	 public String thankYouPage(){
-		 return "thank-you";
-		 
-	 }
-	 
-	  @GetMapping("/access-denied")
-	    public String showAccessDenied() {
-	        return "access-denied";
-	    }
+		return "fancy-login";
+
+	}
+
+	@GetMapping("/")
+	public String welcomePage() {
+		return "welcome";
+	}
+
+	@GetMapping("/thank-you")
+	public String thankYouPage() {
+		return "thank-you";
+
+	}
+
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		return "access-denied";
+	}
 }
--- restaurant/src/main/java/com/myrestaurant/restaurant/config/SecurityConfiguration.java
+++ restaurant/src/main/java/com/myrestaurant/restaurant/config/SecurityConfiguration.java
@@ -13,37 +13,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        @SuppressWarnings("deprecation")
-		User.UserBuilder users = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(users.username("john").password("fun123").roles("OWNER"))
-                .withUser(users.username("mary").password("fun123").roles("WAITER","CUSTOMER"))
-                .withUser(users.username("susan").password("fun123").roles("OWNER","CUSTOMER"));
+    	 User.UserBuilder users = User.withDefaultPasswordEncoder();
+         auth.inMemoryAuthentication()
+    	.withUser(users.username("mary").password("fun123").roles("OWNER"))
+        .withUser(users.username("john").password("fun123").roles("CUSTOMER"))
+        .withUser(users.username("susan").password("test123").roles("WAITER"));
+//      
     }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-        .antMatchers("/").hasAnyRole("WAITER","OWNER","COSTUMER")
+        .antMatchers("/").hasAnyRole("OWNER","CUSTOMER","WAITER")
         .antMatchers("/api6/loginForm").hasAnyRole("OWNER")
-        .antMatchers("/api6/findbills").hasAnyRole("OWNER")
         .antMatchers("/api/login").hasAnyRole("OWNER","WAITER")
-        .antMatchers("/api/findorder").hasAnyRole("OWNER","WAITER")
         .antMatchers("/api1/login").hasAnyRole("OWNER","CUSTOMER")
-        .antMatchers("/api1/save").hasAnyRole("OWNER","CUSTOMER")
-        .antMatchers("/api2/place-order").hasAnyRole("OWNER","CUSTOMER")
-        .antMatchers("/thank-you").hasAnyRole("OWNER","CUSTOMER")
+//        .antMatchers("/").hasAnyRole("WAITER","OWNER","COSTUMER")
+//        .antMatchers("/api6/loginForm").hasAnyRole("OWNER")
+//        .antMatchers("/api6/findbills").hasAnyRole("OWNER")
+//        .antMatchers("/api/login").hasAnyRole("OWNER","WAITER")
+//        .antMatchers("/api/findorder").hasAnyRole("OWNER","WAITER")
+//        .antMatchers("/api1/login").hasAnyRole("OWNER","CUSTOMER")
+//        .antMatchers("/api1/save").hasAnyRole("OWNER","CUSTOMER")
+//        .antMatchers("/api2/place-order").hasAnyRole("OWNER","CUSTOMER")
+//        .antMatchers("/thank-you").hasAnyRole("OWNER","CUSTOMER")
         .and()
-        .formLogin()
-            .loginPage("/api/login")
-            .loginPage("/api6/loginForm")
-            .loginPage("/api1/login")
-            .loginProcessingUrl("/authenticateTheUser")
-            .permitAll()
-            .and()
-                .logout().permitAll();
-//                .and()
-//                .exceptionHandling().accessDeniedPage("/access-denied");
+		.formLogin()
+			.loginPage("/showMyLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/access-denied");
 }
                 
     }
